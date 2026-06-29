ffa831e4353bdac671ff9c2dd93d5ab33de80f9a
BappiGorain <146017226+BappiGorain@users.noreply.github.com>
2026-06-29 14:22:45 +0530

    practiced checkbox

---
+++ Check Box/src/com/checkbox/CheckBox.java
@@ -0,0 +1,63 @@
+package com.checkbox;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CheckBox
+{
+	
+	private WebDriver driver;
+	private ChromeOptions options;
+	private WebDriverWait wait;
+	 
+	
+	@BeforeTest
+	public void setUp()
+	{
+		options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		driver = new ChromeDriver(options);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		driver.manage().window().maximize();
+		driver.navigate().to("https://testautomationpractice.blogspot.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
+	}
+	
+	
+	@Test
+	public void test()
+	{
+		List<WebElement> allCheckBoxes = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='form-group']//input[@type='checkbox']")));
+		
+		for(WebElement element : allCheckBoxes)
+		{
+			element.click();
+			if(element.isSelected())
+			{
+				System.out.println(element.getAttribute("value") + " : is Selected");
+			}
+		}
+	}
+	
+	
+	@AfterTest
+	public void tearDown()
+	{
+		if(driver != null)		
+		{
+			driver.quit();
+		}
+	}	
+}
